687be25e4a5a059de09e0a57fdd61eeeee28ec5b
DesCritas <nik1234nik@gmail.com>
2023-11-24 01:13:59 +0300

    Project created, AppBar added, NavController created

--- app/src/main/java/com/descritas/notesappmvvm/MainActivity.kt
+++ app/src/main/java/com/descritas/notesappmvvm/MainActivity.kt
@@ -1,46 +1,68 @@
 package com.descritas.notesappmvvm
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.descritas.notesappmvvm.navigation.NotesNavHost
 import com.descritas.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
 class MainActivity : ComponentActivity() {
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppMVVMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Scaffold(
+                    topBar = {
+                        Surface(tonalElevation = 12.dp) {
+                            TopAppBar(
+                                title = { Text(text = "Notes App") },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = Blue,
+                                    titleContentColor = White,
+                                )
+                            )
+                        }
+                    },
+                    content = {paddingValues: PaddingValues ->
+
+                        Surface(
+                            modifier = Modifier.fillMaxSize().padding(paddingValues),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            NotesNavHost()
+                        }
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NotesAppMVVMTheme {
-        Greeting("Android")
+
     }
 }
